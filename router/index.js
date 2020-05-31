97e9cdf790e8faf8ce4e1f3e8e0f3887c3e2ff39
aslan-alt <874470891@qq.com>
2020-05-31 14:09:17 +0800

    模块化路由

--- router/index.js
+++ router/index.js
@@ -1,35 +1,88 @@
 const fs = require('fs')
 const url = require('url')
 const path = require('path')
+const ejs = require('ejs')
+
+
+const login = (req,res) =>{
+    ejs.renderFile('./views/login.ejs', {}, (err, data) => {
+        res.writeHead(200, { 'Content-Type': '' });
+        res.end(data)
+    })
+}
+const doLogin = (req,res)=>{
+    let postData = ''
+    req.on('data', (data) => {
+        postData += data
+    })
+    req.on('end', () => {
+        try {
+            res.writeHead(200, { 'Content-Type': 'text/html,charset="utf-8"' });
+            res.end(postData)
+        } catch (error) {
+            console.log(error)
+        }
+    })
+}
+const xxx = (req,res) =>{
+    const { pathname: pathName, query } = url.parse(request.url, true)
+    res.writeHead(200, { 'Content-Type': 'text/html,charset="utf-8"' });
+    res.end(`GET传参成功:${query.name}`)
+}
+
+const news = (req,res)=>{
+    let msg = '数据库里的数据'
+    let list = [
+        { title: '新闻' },
+        { title: '视频' },
+        { title: '音乐' },
+        { title: '衣服' },
+        { title: '谷歌' }
+    ]
+    ejs.renderFile('./views/news.ejs', { msg, list }, (err, data) => {
+        res.writeHead(200, { 'Content-Type': 'text/html,charset="utf-8"' });
+        res.end(data)
+    })
+}
+
+const register = (req,res)=>{
+    res.writeHead(200, { 'Content-Type': 'text/html,charset="utf-8"' });
+    res.end('注册');
+}
+
 
 function getFileMime() {
-    // const types = require('./mime.json')
-
     return JSON.parse(fs.readFileSync('./router/mime.json').toString())
 }
 
 
-exports.static = function (request, response, staticPath) {
+function static(request, response, staticPath) {
     // let query = url.parse(request.url).query //可以通过url模块parse方法拿到url 后面的参数
     let pathName = url.parse(request.url).pathname//可以通过url模块parse方法拿到url 中的路径
     pathName = pathName === '/' ? '/index.html' : pathName
+    console.log('-------------------')
     if (pathName !== '/favicon.ico') {
         const extname = path.extname(pathName)//通过path 模块extname拿到后缀类型
-
-
         try {
-            const data = fs.readFileSync(staticPath + pathName)
-
+            const data = fs.readFileSync('./'+staticPath + pathName)
             if (data) {
                 const contentType = getFileMime()
                 response.writeHead(200, { 'Content-Type': `${contentType[extname]},charset="utf-8"` });
                 response.end(data);
             }
         } catch (error) {
-
+            console.log(error)
         }
-
     }
+}
+
+exports.router = {
+    static,
+    login,
+    doLogin,
+    xxx,
+    news,
+    register
 }
 
 
--- server.js
+++ server.js
@@ -1,66 +1,25 @@
 const http = require('http');
 const url = require('url')
 const fs = require('fs')
-const ejs = require('ejs')
-const static = require('./router/index.js').static //自定义模块，用来返回对应的请求头Content-Type类型
+const router = require('./router/index.js').router
 
 
-http.createServer(function (request, response) {
-    console.log(request.url)
+
+http.createServer(function (req, res) {
+  
     // console.log(request.method)
-    const { pathname: pathName, query } = url.parse(request.url, true)
+    let pathName = url.parse(req.url).pathname
+    pathName = pathName === '/' ? 'static' : pathName.replace('/', '')
+    console.log(pathName)
     // console.log(query)
 
-    static(request, response, './static')
-    if (pathName === '/register') {
-        response.writeHead(200, { 'Content-Type': 'text/html,charset="utf-8"' });
-        response.end('注册');
-    } else if (pathName === '/news') {
-        let msg = '数据库里的数据'
-        let list = [
-            { title: '新闻' },
-            { title: '视频' },
-            { title: '音乐' },
-            { title: '衣服' },
-            { title: '谷歌' }
-        ]
-        ejs.renderFile('./views/news.ejs', { msg, list }, (err, data) => {
-            response.writeHead(200, { 'Content-Type': 'text/html,charset="utf-8"' });
-            response.end(data)
-        })
+    try {
+        router[pathName](req, res,pathName)
+    } catch (error) {
+        res.writeHead(404, { 'Content-Type': 'text/html,charset="utf-8"' });
+        res.end(fs.readFileSync('./static/notFind.html'));
     }
-    else if (pathName === '/login.html') {//登录页面
-        ejs.renderFile('./views/login.ejs', {}, (err, data) => {
-            response.writeHead(200, { 'Content-Type': 'text/html,charset="utf-8"' });
-            response.end(data)
-        })
-    } else if (pathName === '/doLogin') {
-        let postData = ''
-        request.on('data', (data) => {
-            postData += data
-        })
-        request.on('end', () => {
-           
-            try {
-                response.writeHead(200, { 'Content-Type': 'text/html,charset="utf-8"' });
-                response.end(postData)
-            } catch (error) {
-                console.log(error)
-               
-            }
 
-        })
-
-    }
-    else if (pathName === '/query') {
-        response.writeHead(200, { 'Content-Type': 'text/html,charset="utf-8"' });
-        response.end(`GET传参成功:${query.name}`)
-
-    }
-    else {
-        response.writeHead(404, { 'Content-Type': 'text/html,charset="utf-8"' });
-        response.end(fs.readFileSync('./static/notFind.html'));
-    }
 }).listen(8081);
 
 console.log('Server running at http://127.0.0.1:8081/');
